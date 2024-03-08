cbc5d907e8b13fa34f44f7839255f704211f795b
Tom Taylor <tom@tomtaylor.co.uk>
2024-03-08 13:30:37 +0000

    Tweak docs

--- lib/h3_geo.ex
+++ lib/h3_geo.ex
@@ -1,4 +1,13 @@
 defmodule H3Geo do
+  @moduledoc """
+  H3Geo implements the H3 geospatial indexing system.
+
+  It's a wrapper around the h3o Rust library, using `Rustler` to expose
+  functions from the library in a manner that can be easily called from Elixir.
+
+  Currently only a handful of functions are implemented, mostly to do with
+  converting existing geometries into H3 cell indexes.
+  """
   version = Mix.Project.config()[:version]
 
   use RustlerPrecompiled,
@@ -11,11 +20,13 @@ defmodule H3Geo do
     version: version
 
   @type index :: pos_integer()
-  @type precision :: pos_integer()
+  @type precision :: 0..15
 
   @doc """
-  Takes a `Geo.Point` and an `Integer` precision and returns an integer
+  Takes a `Geo.Point` and an integer precision and returns an integer
   representing the H3 Cell.
+
+  [Rust documentation](https://docs.rs/h3o/latest/h3o/struct.LatLng.html#method.to_cell).
   """
   @spec point_to_cell(Geo.Point.t(), precision()) ::
           {:ok, pos_integer()} | {:error, :invalid_lat_lng | :invalid_resolution}
@@ -25,7 +36,11 @@ defmodule H3Geo do
   Takes `Geo.Polygon` and an integer precision and returns a list of integers
   representing the H3 cells that intersect with the polygon.
 
-  Use containment mode, so the cells returned fully cover the polygon.
+  Uses the
+  [Covers](https://docs.rs/h3o/latest/h3o/geom/enum.ContainmentMode.html#variant.Covers)
+  containment mode, so the cells returned fully cover the polygon.
+
+  [Rust documentation](https://docs.rs/h3o/latest/h3o/geom/trait.ToCells.html)
   """
   @spec polygon_to_cells(Geo.Polygon.t(), precision()) ::
           {:ok, list(index())} | {:error, :invalid_resolution | :invalid_geometry}
@@ -35,7 +50,11 @@ defmodule H3Geo do
   Takes `Geo.MultiPolygon` and an integer precision and returns a list of
   integers representing the H3 cells that intersect with the multipolygon.
 
-  Use containment mode, so the cells returned fully cover the multipolygon.
+  Uses the
+  [Covers](https://docs.rs/h3o/latest/h3o/geom/enum.ContainmentMode.html#variant.Covers)
+  containment mode, so the cells returned fully cover the multipolygon.
+
+  [Rust documentation](https://docs.rs/h3o/latest/h3o/geom/trait.ToCells.html)
   """
   @spec multipolygon_to_cells(Geo.MultiPolygon.t(), precision()) ::
           {:ok, list(index())} | {:error, :invalid_resolution | :invalid_geometry}
@@ -45,6 +64,8 @@ defmodule H3Geo do
   Takes a list of indexes and returns the compact indexes.
 
   The incoming list is filtered for unique values automatically.
+
+  [Rust documentation](https://docs.rs/h3o/latest/h3o/struct.CellIndex.html#method.compact)
   """
   @spec compact(list(index())) ::
           {:ok, list(index())} | {:error, :invalid_cell_index | :compaction_error}
@@ -52,6 +73,8 @@ defmodule H3Geo do
 
   @doc """
   Takes a list of indexes and returns the uncompacted indexes at the desired precision.
+
+  [Rust documentation](https://docs.rs/h3o/latest/h3o/struct.CellIndex.html#method.uncompact)
   """
   @spec uncompact(list(index()), precision()) ::
           {:ok, list(index())} | {:error, :invalid_cell_index | :invalid_resolution}
